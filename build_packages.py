6ef0980b7382037595fa4e5d7069e77d0e9e02a2
Ed Schouten <ed@nuxi.nl>
2015-09-09 23:35:09 +0200

    Add a license to the code.

--- build_packages.py
+++ build_packages.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+# Copyright (c) 2015 Nuxi, https://nuxi.nl/
+#
+# This file is distrbuted under a 2-clause BSD license.
+# See the LICENSE file for details.
 
 import os
 import sys
--- src/catalog_set.py
+++ src/catalog_set.py
@@ -1,3 +1,8 @@
+# Copyright (c) 2015 Nuxi, https://nuxi.nl/
+#
+# This file is distrbuted under a 2-clause BSD license.
+# See the LICENSE file for details.
+
 import os
 
 from . import util
