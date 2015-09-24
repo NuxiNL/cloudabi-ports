7a5753700ba2a9ef4106522c6edf0f60873d26ca
Ed Schouten <ed@nuxi.nl>
2015-09-24 17:57:35 +0200

    Also depend on help2man.

--- src/package.py
+++ src/package.py
@@ -96,7 +96,8 @@ class TargetPackage:
         # Perform the build inside a buildroot with its dependencies
         # installed in place.
         self.initialize_buildroot({
-            'autoconf', 'binutils', 'bison', 'cmake', 'llvm', 'make', 'pkgconf'
+            'autoconf', 'binutils', 'bison', 'cmake', 'help2man',
+            'llvm', 'make', 'pkgconf',
         }, self._lib_depends)
         print('BUILD', self._name)
         self._build_cmd(
