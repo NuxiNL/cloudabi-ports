20a27be562fca6849f5a351c3a28bef68b93d222
Ed Schouten <ed@nuxi.nl>
2016-09-23 11:19:57 +0200

    Simplify reformat.sh to use git ls-files.
    The existing script is broken in the sense that due to the missing
    parentheses, we don't properly reformat the *.py files. It also attempts
    to reformat the files stored in _obj/, which it shouldn't.

--- reformat.sh
+++ reformat.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-find . -name '*.py' -o -name BUILD -print0 | \
-  xargs -0 -n1 -P8 autopep8 --aggressive --aggressive --aggressive --in-place
+git ls-files '*.py' '*/BUILD' | \
+  xargs -n1 -P8 autopep8 --aggressive --aggressive --aggressive --in-place
