cef8e25a53dd06982603f70e8d46a3799ead66ee
Ed Schouten <ed@nuxi.nl>
2016-12-28 17:13:14 +0100

    Replace 'signal' and 'subprocess' by empty stubs.
    We cannot expect those modules to work in any way on CloudABI, but lots
    of Python code ends up importing them anyway, even if they are only
    interested in their functions in some odd cases.

---
+++ packages/python/Lib/subprocess.py
@@ -0,0 +1,4 @@
+# The upstream version of this source file does not work with this
+# version of Python, as it makes use of various APIs that are unavailable.
+# Provide an empty stub, so that existing imports of this module
+# continue to work.
