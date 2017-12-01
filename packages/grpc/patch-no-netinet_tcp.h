1dfe2077955a5a841262cdefb5e81033fd1fd609
Ed Schouten <ed@nuxi.nl>
2017-12-01 17:13:57 +0100

    Upgrade gRPC to version 1.7.2.

--- packages/grpc/patch-no-netinet_tcp.h
+++ packages/grpc/patch-no-netinet_tcp.h
@@ -1,6 +1,6 @@
 --- src/core/lib/iomgr/socket_utils_common_posix.c
 +++ src/core/lib/iomgr/socket_utils_common_posix.c
-@@ -43,7 +43,9 @@
+@@ -28,7 +28,9 @@
  #include <fcntl.h>
  #include <limits.h>
  #include <netinet/in.h>
@@ -12,7 +12,7 @@
  #include <sys/socket.h>
 --- src/core/lib/iomgr/tcp_server_posix.c
 +++ src/core/lib/iomgr/tcp_server_posix.c
-@@ -45,7 +45,9 @@
+@@ -30,7 +30,9 @@
  #include <errno.h>
  #include <fcntl.h>
  #include <netinet/in.h>
@@ -24,7 +24,7 @@
  #include <sys/stat.h>
 --- src/core/lib/iomgr/udp_server.c
 +++ src/core/lib/iomgr/udp_server.c
-@@ -46,7 +46,9 @@
+@@ -31,7 +31,9 @@
  #include <fcntl.h>
  #include <limits.h>
  #include <netinet/in.h>
--- packages/grpc/patch-string.h
+++ packages/grpc/patch-string.h
@@ -1,11 +1,11 @@
 --- src/core/lib/surface/init.c
 +++ src/core/lib/surface/init.c
-@@ -34,7 +34,7 @@
+@@ -19,7 +19,7 @@
  #include <grpc/support/port_platform.h>
  
  #include <limits.h>
 -#include <memory.h>
 +#include <string.h>
  
+ #include <grpc/fork.h>
  #include <grpc/grpc.h>
- #include <grpc/support/alloc.h>
